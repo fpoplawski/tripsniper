0c1af52a03dfa2241678723536e7b8895e4c1ecb
fpoplawski <f.poplawski1029@gmail.com>
2025-06-20 17:36:57 +0200

    Add Booking.com fetcher

--- src/trip_sniper/fetchers/__init__.py
+++ src/trip_sniper/fetchers/__init__.py
@@ -1,6 +1,7 @@
 """Fetchers package."""
 
+from .booking import BookingFetcher
 from .skyscanner import SkyscannerFetcher
 
-__all__ = ["SkyscannerFetcher"]
+__all__ = ["BookingFetcher", "SkyscannerFetcher"]
 
